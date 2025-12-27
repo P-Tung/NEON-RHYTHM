60e67f9b7ab6665299f402b2df11d2c971ead75b
TungPhamQ <tungpham2109@gmail.com>
2025-12-27 18:54:57 +0700

    fix: Reduce recording timeout duration from 5000ms to 3500ms for improved responsiveness in stopping recordings

--- App.tsx
+++ App.tsx
@@ -255,7 +255,7 @@ const App: React.FC = () => {
       const timeoutId = setTimeout(() => {
         stopRecording();
         stopRecordingTimeoutRef.current = null;
-      }, 5000);
+      }, 3500);
 
       stopRecordingTimeoutRef.current = timeoutId as any;
 
